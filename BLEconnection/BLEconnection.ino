4dbbf233db7a23866dc85ba892638e2b2985f871
Banri Yasui <69246274+jomspk@users.noreply.github.com>
2021-09-20 17:33:01 +0900

    最終変更
    接続が切れたらESP32の処理をリスタートする

--- BLEconnection/BLEconnection.ino
+++ BLEconnection/BLEconnection.ino
@@ -7,6 +7,8 @@
 #define DEVICE_NAME "ESP32" //デバイス名
 #define SPI_SPEED 115200　//SPI通信速度
 #define GREEN_BUTTON_PIN 32
+#define RED_BUTTON_PIN 33
+#define BLUE_BUTTON_PIN 27
 
 /*UUID定義*/
 #define SERVICE_UUID           "28b0883b-7ec3-4b46-8f64-8559ae036e4e"  // サービスのUUID
@@ -14,8 +16,9 @@
 
 /*通信制御用*/
 BLECharacteristic *pCharacteristicTX; //送信用キャラクタリスティック
-bool deviceDisconnected = false;
+bool deviceDisconnected = false; //デバイスの切断状態
 bool deviceConnected = false; //デバイスの接続状態
+int send_value = 0; //iOSに送る値
 
 /*プッシュボタン*/
 int buttonState = LOW; //状態
@@ -31,6 +34,7 @@ const float iIntervalTime = 0.1; //計測間隔(1秒)
 //接続・切断時コールバック
 class funcServerCallbacks: public BLEServerCallbacks{
   void onConnect(BLEServer* pServer){
+    Serial.println("Connected");
     deviceConnected = true;
     deviceDisconnected = false;
     }
@@ -49,6 +53,8 @@ static void kickRoutine(){
 void doInitialize(){
   Serial.begin(115200);
   pinMode(GREEN_BUTTON_PIN, INPUT_PULLDOWN);
+  pinMode(RED_BUTTON_PIN, INPUT_PULLDOWN);
+  pinMode(BLUE_BUTTON_PIN, INPUT_PULLDOWN);
 }
 
 /*準備処理*/
@@ -66,34 +72,23 @@ void doMainProcess(){
   //緑のボタンのステータスを読む
   char str[30];
   int green_state = digitalRead(GREEN_BUTTON_PIN);
-  sprintf(str,"%d",green_state);
+  int red_state = digitalRead(RED_BUTTON_PIN);
+  int blue_state = digitalRead(BLUE_BUTTON_PIN);
+  if(green_state){
+    send_value = 1;  
+  }
+  if(red_state){
+    send_value = 2;  
+  }
+  if(blue_state){
+    send_value = 3;  
+  }
+  sprintf(str,"%d",send_value);
   Serial.println(str);
   pCharacteristicTX->setValue(str);
   pCharacteristicTX->notify();  
 }
 
-void resetup(){
-  // 初期化処理を行ってBLEデバイスを初期化する
-  doInitialize();
-  BLEDevice::init(DEVICE_NAME);
-
-  //Serverオブジェクトを作成してコールバックを設定する
-  BLEServer *pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new funcServerCallbacks());
-
-  //Serviceオブジェクトを作成して準備処理を実行する
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  doPrepare(pService);
-
-  //サービスを開始して、SERVICE_UUIDでアドバタイジングを開始する
-  pService->start();
-  BLEAdvertising *pAdvertising = pServer->getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->start();
-  //タイマー割り込みを開始する
-  ticker.attach(iIntervalTime, kickRoutine);
-  Serial.println("Waiting to connect...");    
-}
 void setup() {
   // 初期化処理を行ってBLEデバイスを初期化する
   doInitialize();
@@ -127,9 +122,7 @@ void loop() {
     }
   }
   if(deviceDisconnected){
-    resetup();
-    deviceDisconnected = false;
-    deviceConnected = false;
-    bReadyTicker = false;        
+    ESP.restart();
+    delay(10000);   
   } 
 }
